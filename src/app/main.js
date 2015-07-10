afe03d0c31dffe33addd9953a8a8cf2f32e0af19
Jeff Parr <jeffparr100@gmail.com>
2015-07-10 13:48:50 -0700

    add building types to los angeles

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,9 @@
 require.config({
   paths: {
-    jquery: 'lib/jquery',
-    underscore: 'lib/underscore',
-    backbone: 'lib/backbone',
-    d3: 'lib/d3',
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min',
+    underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
+    backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.1/backbone-min',
+    d3: '//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min',
     ionrangeslider: 'lib/ion.rangeSlider',
     templates: '../templates'
   }
